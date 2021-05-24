53aaf92ec06105b9acd9d7cc3928123e8bd069d0
ginestah <68362184+ginestah@users.noreply.github.com>
2021-05-24 12:19:52 -0600

    valid anagram ruby

---
+++ valid_anagram/valid_anagram.rb
@@ -0,0 +1,13 @@
+def is_anagram(s, t)
+  #easy check to eliminate strings of different lengths so there is no iteration
+  return false if s.length != t.length
+  # count each letter in s and see if same number of letters in t
+  for i in 'a'..'z'
+      if s.count(i) != t.count(i)
+          return false
+      end
+  end
+  return true
+end
+
+# contraints said all strings are lowercase, or would have to adapt solution a bit
